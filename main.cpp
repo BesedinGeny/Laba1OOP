002346a2f94b9a3a46c686bf52deb208f630932b
Eugen Besedin <besedin_geny@mail.ru>
2019-09-13 22:26:03 +0700

    Wave

--- main.cpp
+++ main.cpp
@@ -3,79 +3,52 @@
 using namespace std;
 
 typedef struct{
-    int Z; // вещественная часть
-    float R; // дробная часть
-} Number;
+    float frequency; // вещественная часть
+    float Length; // дробная часть
+} Wave;
 
-int Init(Number *k, int a1, float a2){
-    cout << "intitonalize" << endl;
-    k->Z = a1;
-    k->R = a2;
+int Init(Wave *k, float a1, float a2){
+    cout << "intitonalized" << endl;
+    k->frequency = a1;
+    k->Length = a2;
     return 0;
 }
 
-int Read(Number *k){
+int Read(Wave *k){
     cout << "Input Int part, then float part: " << endl;
-    cin >> k->Z;
-    cin >> k->R;
+    cin >> k->frequency;
+    cin >> k->Length;
     return 0;
 }
 
-int Display(Number k){
-    cout << "Number is " << k.Z << " + " << k.R << endl;
+int Display(Wave k){
+    cout << "Wave`s frequency is " << k.frequency << " and length is " << k.Length << endl;
     return 0;
 }
 
-Number Add(Number k, Number t){
-    Number Answ;
-    Answ.R = k.R + t.R;
-    Answ.Z = k.Z + t.Z;
-    if (Answ.R >= 1.0) {
-        Answ.R -= 1;
-        Answ.Z += 1;
-    }
+Wave Add(Wave k, Wave t){
+    Wave Answ;
+    Answ.Length = k.Length + t.Length;
+    Answ.frequency = k.frequency + t.frequency;
+
     return Answ;
 }
 
-Number Round(Number k){
-    Number answ;
-    if (k.R >= 0.5)
-    {
-        answ.Z = k.Z + 1;
-        answ.R = 0;
-    }
-    else
-    {
-        answ.Z = k.Z;
-        answ.R = 0;
-    }
-    return answ;
+float Period(Wave k){
+   return 1. / k.frequency;
 }
 
-int Cmp(Number k, Number t){
-    if (k.Z > t.Z){
-        return 1;
-    }
-    else
-        if (k.Z < t.Z){
-            return 2;
-        }
-    else
-            if (k.R >= t.R ){
-                return 1;
-            }
-    else return 2;
-}
+
 
 int main()
 {
-    Number example1, example2;
+    Wave example1, example2;
     Init(&example2, 0, 0.1);
     Read(&example1);
     Display(example1);
     Display(Add(example1, example2));
-    Display(Round(example1));
-    Cmp(example1, example2)==1?cout << "First ":cout <<"Second ";
-    cout << "is bigger" << endl;
+    cout << "Period of Wave 1 is " << Period(example1) << endl;
+    //Cmp(example1, example2)==1?cout << "First ":cout <<"Second ";
+    //cout << "is bigger" << endl;
     return 0;
 }
