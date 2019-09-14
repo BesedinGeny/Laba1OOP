d7d6dd0ec93c1541c1e9c103a26325c90e9df3a3
Eugen Besedin <besedin_geny@mail.ru>
2019-09-14 18:01:36 +0700

    My Func with OOp

--- main.cpp
+++ main.cpp
@@ -2,55 +2,45 @@
 
 using namespace std;
 
-class Number{
+class Wave{
 private:
-    int Z;
-    float R;
+    float frequency;
+    float Length;
 public:
-    void Init(int a,float b){
-        Z = a;
-        R = b;
+    void Init(float a,float b){
+        frequency = a;
+        Length = b;
     }
     void Read(){
         cout << "Input Int part, then float part: " << endl;
-        cin >> Z;
-        cin >> R;
+        cin >> frequency;
+        cin >> Length;
     }
     void Display(){
-        cout << "Number is " << Z << " + " << R << endl;
+        cout << "Number is " << frequency << " + " << Length << endl;
     }
-    Number Add(Number k){
-        k.Z += this->Z;
-        k.R += this->R;
-        if (k.R >= 1.0){
-            k.Z += 1;
-            k.R -= 1;
-        }
-        return k;
-    }
-    Number Round(){
-        Number answ;
-        answ.Init(this->Z, this->Z);
-        if (R >= 0.5){
-            answ.Z += 1;
-        }
-        answ.R = 0;
-        return answ;
+    Wave Add( Wave t){
+        Wave Answ;
+        Answ.Length = this->Length + t.Length;
+        Answ.frequency = this->frequency + t.frequency;
+
+        return Answ;
     }
 
+    float Period(){
+       return 1. / frequency;
+    }
 };
 
 int main()
 {
-    Number one, two, three;
+    Wave one, two, three;
     one.Init(1, 0.5);
     two.Read();
     one.Display();
     two.Display();
     three = one.Add(two);
     three.Display();
-    Number four = three.Round();
-    three.Display();
-    four.Display();
+    cout << "Periof of third wave is " << three.Period();
     return 0;
 }
