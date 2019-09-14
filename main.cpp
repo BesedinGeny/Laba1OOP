0b9076292b9d138a003cb153782f26d33ae28fcb
Eugen Besedin <besedin_geny@mail.ru>
2019-09-14 17:36:34 +0700

    OOP def funcs

--- main.cpp
+++ main.cpp
@@ -2,52 +2,55 @@
 
 using namespace std;
 
-typedef struct{
-    float frequency; // вещественная часть
-    float Length; // дробная часть
-} Wave;
+class Number{
+private:
+    int Z;
+    float R;
+public:
+    void Init(int a,float b){
+        Z = a;
+        R = b;
+    }
+    void Read(){
+        cout << "Input Int part, then float part: " << endl;
+        cin >> Z;
+        cin >> R;
+    }
+    void Display(){
+        cout << "Number is " << Z << " + " << R << endl;
+    }
+    Number Add(Number k){
+        k.Z += this->Z;
+        k.R += this->R;
+        if (k.R >= 1.0){
+            k.Z += 1;
+            k.R -= 1;
+        }
+        return k;
+    }
+    Number Round(){
+        Number answ;
+        answ.Init(this->Z, this->Z);
+        if (R >= 0.5){
+            answ.Z += 1;
+        }
+        answ.R = 0;
+        return answ;
+    }
 
-int Init(Wave *k, float a1, float a2){
-    cout << "intitonalized" << endl;
-    k->frequency = a1;
-    k->Length = a2;
-    return 0;
-}
-
-int Read(Wave *k){
-    cout << "Input Int part, then float part: " << endl;
-    cin >> k->frequency;
-    cin >> k->Length;
-    return 0;
-}
-
-int Display(Wave k){
-    cout << "Wave`s frequency is " << k.frequency << " and length is " << k.Length << endl;
-    return 0;
-}
-
-Wave Add(Wave k, Wave t){
-    Wave Answ;
-    Answ.Length = k.Length + t.Length;
-    Answ.frequency = k.frequency + t.frequency;
-
-    return Answ;
-}
-
-float Period(Wave k){
-   return 1. / k.frequency;
-}
-
-
+};
 
 int main()
 {
-    Wave example1, example2;
-    Init(&example2, 0, 0.1);
-    Read(&example1);
-    Display(example1);
-    Display(Add(example1, example2));
-    cout << "Period of Wave 1 is " << Period(example1) << endl;
-
+    Number one, two, three;
+    one.Init(1, 0.5);
+    two.Read();
+    one.Display();
+    two.Display();
+    three = one.Add(two);
+    three.Display();
+    Number four = three.Round();
+    three.Display();
+    four.Display();
     return 0;
 }
